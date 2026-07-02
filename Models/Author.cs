ba3a9dcd22d1b50bf1aded1dfff8d51095d09d79
Margreet-Emil <147156878+Margreet-Emil@users.noreply.github.com>
2026-07-02 19:45:52 +0300

    Delete Models directory

--- Models/Author.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Author
-{
-    public int AuthorID { get; set; }
-    public string AuthorName { get; set; } = string.Empty;
-    public ICollection<Book> Books { get; set; } = new List<Book>();
-
-  
-}
--- Models/Category.cs
+++
@@ -1,7 +0,0 @@
-﻿
-public class Category
-{
-    public int CategoryID { get; set; }
-    public string CName { get; set; } = string.Empty;
-    public ICollection<Book> Books { get; set; } = new List<Book>();
-}
